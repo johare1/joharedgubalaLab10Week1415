047868f7f80d6be51323fc87207481c27a0f9f1c
Jack OHare <johare@luc.edu>
2017-12-13 04:55:50 +0000

    finished traversal of graph

--- src/main/java/edu/luc/cs271/countrygraph/Main.java
+++ src/main/java/edu/luc/cs271/countrygraph/Main.java
@@ -1,16 +1,16 @@
 package edu.luc.cs271.countrygraph;
 
 import java.util.*;
-
+import org.jgrapht.*;
+import org.jgrapht.graph.*;
+import org.jgrapht.traverse.*;
 
 public class Main {
 
   public static void main(final String[] args) {
 
-     final Graph<String, DefaultEdge> myMap = new SimpleGraph<>(DefaultEdge.class);
+    final Graph<String, DefaultEdge> myMap = new SimpleGraph<>(DefaultEdge.class);
 
-
-      
     final String HU = countryCodes.HU;
     final String UA = countryCodes.UA;
     final String SK = countryCodes.SK;
@@ -35,29 +35,100 @@ public class Main {
     myMap.addEdge(SK, AT);
     myMap.addEdge(HR, RS);
     myMap.addEdge(RS, RO);
+
+    // print resulting graph
+    System.out.print(myMap);
+
+    final Iterator<String> breadthfirst = new BreadthFirstIterator<>(myMap, HU);
+
+    while (breadthfirst.hasNext()) {
+
+      final String country = breadthfirst.next();
+
+      System.out.println("BreadthFirst - ");
+      System.out.println(country);
+    }
     
+    final Iterator<String> breadthfirst2 = new BreadthFirstIterator<>(myMap, RO);
 
+    while (breadthfirst2.hasNext()) {
 
-    myMap.addEdge(IL, IN);
+      final String country = breadthfirst2.next();
 
-    GeoGraph.addEdge(IL, KY);
+      System.out.println("BreadthFirst2 - ");
+      System.out.println(country);
+    }
 
-    GeoGraph.addEdge(IL, MO);
+    final Iterator<String> closestfirst = new ClosestFirstIterator<>(myMap, HU);
 
-    GeoGraph.addEdge(IL, WI);
+    while (closestfirst.hasNext()) {
 
-    GeoGraph.addEdge(IL, IA);
+      final String country = closestfirst.next();
 
-    GeoGraph.addEdge(IN, KY);
+      System.out.println("ClosestFirst - ");
+      System.out.println(country);
+    }
+    
+    final Iterator<String> closestfirst2 = new ClosestFirstIterator<>(myMap, RO);
 
-    GeoGraph.addEdge(KY, MO);
+    while (closestfirst2.hasNext()) {
 
-    GeoGraph.addEdge(MO, IA);
+      final String country = closestfirst2.next();
 
-    GeoGraph.addEdge(IA, WI);
-     
-     
-     
-     
+      System.out.println("ClosestFirst2 - ");
+      System.out.println(country);
+    }
+
+    final Iterator<String> depthfirst = new DepthFirstIterator<>(myMap, HU);
+
+    while (depthfirst.hasNext()) {
+
+      final String country = depthfirst.next();
+
+      System.out.println("DepthFirst - ");
+      System.out.println(country);
+    }
+    
+    final Iterator<String> depthfirst2 = new DepthFirstIterator<>(myMap, RO);
+
+    while (depthfirst2.hasNext()) {
+
+      final String country = depthfirst2.next();
+
+      System.out.println("DepthFirst2 - ");
+      System.out.println(country);
+    }
+    
+    
+    final Iterator<String> randomwalk = new RandomWalkIterator<>(myMap, HU, false, 8);
+
+    while (randomwalk.hasNext()) {
+
+      final String country = randomwalk.next();
+
+      System.out.println("RandomWalk - ");
+      System.out.println(country);
+      
+      
+    }
+    
+    final Iterator<String> randomwalk2 = new RandomWalkIterator<>(myMap, RO, false, 8);
+
+    while (randomwalk2.hasNext()) {
+
+      final String country = randomwalk2.next();
+
+      System.out.println("RandomWalk2 - ");
+      System.out.println(country);
+      
+      
+    }
+    
+    
+    GreedyColoring GCMap = new GreedyColoring(myMap);
+
+    System.out.println(GCMap.getColoring());
+    
+    
   }
 }
