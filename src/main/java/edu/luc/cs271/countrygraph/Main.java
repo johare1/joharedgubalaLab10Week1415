44ce8f49b9198be1149cafaa239953a0810bab25
Jack OHare <johare@luc.edu>
2017-12-13 04:05:01 +0000

    add country codes and del old files

---
+++ src/main/java/edu/luc/cs271/countrygraph/Main.java
@@ -0,0 +1,12 @@
+package edu.luc.cs271.countrygraph;
+
+import java.util.*;
+import java.util.Scanner;
+
+public class Main {
+
+  public static void main(final String[] args) {
+
+    final Scanner input = new Scanner(System.in);
+  }
+}
---
+++ src/main/java/edu/luc/cs271/countrygraph/countryCodes.java
@@ -0,0 +1,12 @@
+package edu.luc.cs271.countrygraph;
+
+public class countryCodes {
+
+  static final String HU = "Hungary";
+  static final String UA = "Ukraine";
+  static final String SK = "Slovakia";
+  static final String AT = "Austria";
+  static final String RO = "Romania";
+  static final String HR = "Croatia";
+  
+}
